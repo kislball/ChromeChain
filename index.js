9cecdf406d973e8186114a4e50c55dcb2989aa30
Alice <alicediscord101@gmail.com>
2019-12-29 22:08:26 +0700

    Some changes

--- index.js
+++ index.js
@@ -23,9 +23,14 @@ SOFTWARE.
 */
 
 const puppeteer = require('puppeteer');
-const http = require('http');
 const fetch = require('node-fetch');
+const http = require('http');
 const url = require('url');
+
+var WAIT_UNTIL = process.env.WAIT_UNTIL || 'domcontentloaded';
+var PORT = process.env.PORT || 80;
+var PROXY = process.env.PROXY;
+
 
 class ChromeChain {
   constructor() {
@@ -33,10 +38,9 @@ class ChromeChain {
   }
 
   async start() {
-    let proxy = "proxy-here-if-needs";
     this.browser = await puppeteer.launch({
       args: [
-        //`--proxy-server=${proxy}`, // Use proxy if you want to use ChromeChain on your server
+        //`--proxy-server=${PROXY}`,
         '--no-sandbox',
         '--disable-setuid-sandbox'
       ],
@@ -50,7 +54,7 @@ class ChromeChain {
   async takeScreenshot(url) {
     url = url.startsWith("http") ? url : `http://${url}`;
     let page = await this.browser.newPage();
-    await page.goto(url);
+    await page.goto(url, {waitUntil: WAIT_UNTIL});
     let shot = await page.screenshot();
     await page.close();
     return shot;
@@ -62,33 +66,47 @@ async function main() {
   const Puppet = new ChromeChain();
 
   http.createServer((req, res) => {
-    var args = url.parse(req.url, true).query;
-    
-    console.log(`[REQUEST] ${req.connection.remoteAddress} -> ${args.url}`);
-    Puppet.takeScreenshot(args.url).then(data  => {
-      fetch("https://api.imgur.com/3/image", {
-        "headers": {
-          "Authorization": "Client-ID 6656d64547a5031",
-          "Content-Type": "application/json"
-        },
-        "method": "POST",
-        "body": data
-      })
-      .then(resp => resp.json())
-      .then(json => {
-        let msg = {"msg": "OK", "content": json.data.link};
-        res.writeHead(200, {"Content-Type": "application/json"})
-        res.write(JSON.stringify(msg));
-        res.end(null);
-      });
-    }).catch(err => {
-      let msg = {"msg": "There is an error", "content": `${err}`};
-      res.writeHead(500, {"Content-Type": "application/json"});
+    var args = url.parse(req.url, true).query || null;
+
+    if (!args) {
+      
+      let msg = {"msg": "ERR", "content": "Provide a target URL, please"};
+      res.writeHead(200, {"Content-Type": "application/json"});
       res.write(JSON.stringify(msg));
       res.end(null);
-    });
-  }).listen(process.env.PORT);
+      
+    } else {
+      
+      Puppet.takeScreenshot(args.url).then(data  => {
+        
+        fetch("https://api.imgur.com/3/image", {
+          "headers": {
+            "Authorization": "Client-ID 6656d64547a5031",
+            "Content-Type": "application/json"
+          },
+          "method": "POST",
+          "body": data
+        })
+        .then(resp => resp.json())
+        .then(json => {
+          
+          let msg = {"msg": "OK", "content": json.data.link};
+          res.writeHead(200, {"Content-Type": "application/json"})
+          res.write(JSON.stringify(msg));
+          res.end(null);
+          
+        });
+      }).catch(err => {
+        
+        let msg = {"msg": "ERR", "content": `${err}`};
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.write(JSON.stringify(msg));
+        res.end(null);
+        
+      });
+    }
+  }).listen(PORT);
 }
 
 main();
-console.log(`Listening on the port ${process.env.PORT}`);
+console.log(`[OK] Listening on the port ${PORT}`);
