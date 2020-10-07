33e474dd69ea4d3a49de248f6c347aae7d874bb7
Nelly_ <6546381-NellyD3v@users.noreply.gitlab.com>
2020-10-08 01:26:26 +0700

    Lot of changes, commited to the GitLab. Added cache, cooldown, allowed CORS and more

--- index.js
+++ index.js
@@ -23,9 +23,42 @@ SOFTWARE.
 */
 
 const puppeteer = require('puppeteer');
-const fetch = require('node-fetch');
 const http = require('http');
 const url = require('url');
+
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+const COOLDOWN = process.env.COOLDOWN || 2000;
+const CACHE_DIR = 'cache';
+const WAIT_BEFORE_SS = process.env.WAIT_BEFORE_SCREENSHOT || 2000;
+const PROXY_SERVER = process.env.PROXY;
+
+var busy = [];
+
+
+// Check if cache directory exists
+if (!fs.existsSync(CACHE_DIR)) {
+  console.log('Making a cache directory because it doest not exist.')
+  fs.mkdirSync(CACHE_DIR);
+
+} else {
+  // Remove all cached screenshots on startup
+  // if cache directory if exists
+  fs.readdir(CACHE_DIR, (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    for (const file of files) {
+      fs.unlink(path.join(CACHE_DIR, file), err => {
+        if (err) throw err;
+      });
+    }
+  });
+}
 
 
 class ChromeChain {
@@ -35,23 +68,35 @@ class ChromeChain {
 
   async start() {
     let args = ['--no-sandbox', '--disable-setuid-sandbox'];
-    if (process.env.PROXY) args.add(`--proxy-server=${process.env.PROXY}`);
+    if (process.env.PROXY) args.push(`--proxy-server=${PROXY_SERVER}`);
+
     this.browser = await puppeteer.launch({
       args: args,
       defaultViewport: {
-        width: 1366,
-        height: 768
+        width: Number(process.env.SCREENSHOT_WIDTH) || 1366,
+        height: Number(process.env.SCREENSHOT_HEIGHT) || 768
       }
     });
   }
 
-  async takeScreenshot(url) {
-    url = url.startsWith("http") ? url : `http://${url}`;
-    let page = await this.browser.newPage();
-    await page.goto(url, {waitUntil: 'domcontentloaded'});
-    let shot = await page.screenshot();
+  async takeScreenshot(url_) {
+    var page = await this.browser.newPage();
+    await page.goto(url_, {waitUntil: 'load'});
+
+    let get_binary = () => new Promise(function (res, rej) {
+      setTimeout(async () => {
+        page.screenshot().then(binary => {
+          res(binary);
+        }).catch(err => {
+          rej(err);
+        });
+      }, WAIT_BEFORE_SS);
+    });
+
+    let binary = await get_binary();
     await page.close();
-    return shot;
+
+    return binary;
   }
 }
 
@@ -60,47 +105,89 @@ async function main() {
   const Puppet = new ChromeChain();
 
   http.createServer((req, res) => {
-    var args = url.parse(req.url, true).query || null;
 
-    if (!args.url) {
-      
-      let msg = {"msg": "ERR", "content": "Provide a target URL, please"};
-      res.writeHead(200, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
+    var args = url.parse(req.url, true).query || null;
+    if (!args.url) return;
+
+    args.url = args.url.startsWith("http") ? args.url : `http://${args.url}`;
+    var base_url = args.url.replace('http://', '').replace('https://', '');
+
+    console.log(`[INFO] ${req.connection.remoteAddress} requested screenshot of '${args.url}'`)
+
+    if (!Puppet.browser) {
+
+      let msg = {"msg": "ERR", "content": "Chromium is starting up. Please, make your request later."};
+      res.writeHead(403, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
       res.write(JSON.stringify(msg));
       res.end(null);
-      
+      return;
+
+    } else if (!args.url) {
+
+      let msg = {"msg": "ERR", "content": "Provide a target URL, please"};
+      res.writeHead(404, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
+      res.write(JSON.stringify(msg));
+      res.end(null);
+
     } else {
-      
-      Puppet.takeScreenshot(args.url).then(data  => {
-        
-        fetch("https://api.imgur.com/3/image", {
-          "headers": {
-            "Authorization": "Client-ID 6656d64547a5031",
-            "Content-Type": "application/json"
-          },
-          "method": "POST",
-          "body": data
-        })
-        .then(resp => resp.json())
-        .then(json => {
-          
-          let msg = {"msg": "OK", "content": json.data.link};
-          res.writeHead(200, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"})
-          res.write(JSON.stringify(msg));
-          res.end(null);
-          
-        });
+
+      if (busy.includes(req.connection.remoteAddress)) {
+
+        let msg = {"msg": "ERR", "content": "You are on cooldown. Please, be patient."};
+        res.writeHead(403, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
+        res.write(JSON.stringify(msg));
+        res.end(null);
+        return;
+
+      } else {
+        busy.push(req.connection.remoteAddress);
+      }
+
+      var CURRENT_SS_PATH = `${CACHE_DIR}/${base_url.replace(/\//g, '__')}.png`;
+
+      // Check if current website is in cache
+      if (fs.existsSync(CURRENT_SS_PATH)) {
+        console.log(`Screenshot of ${base_url} exists in cache.`)
+
+        // Send image from cache instead of taking a new screenshot
+        // to avoid ban from website
+        bytes = fs.readFileSync(CURRENT_SS_PATH);
+
+        res.writeHead(200, {"Content-Type": "image/png", "Access-Control-Allow-Origin": "*"})
+        res.write(bytes)
+        res.end(null);
+
+        setTimeout(() => busy.splice(busy.indexOf(req.connection.remoteAddress), 1), COOLDOWN);
+
+        return;
+      }
+
+      Puppet.takeScreenshot(args.url).then(bytes => {
+        console.log(`Screenshot of ${base_url} does not exists in cache.`)
+
+        // write screenshot into cache file to avoid multiple requests
+        // to the given resource
+        fs.writeFileSync(CURRENT_SS_PATH, bytes);
+
+        res.writeHead(200, {"Content-Type": "image/png", "Access-Control-Allow-Origin": "*"})
+        res.write(bytes)
+        res.end(null);
+
+        setTimeout(() => busy.splice(busy.indexOf(req.connection.remoteAddress), 1), COOLDOWN);
+
       }).catch(err => {
         
         let msg = {"msg": "ERR", "content": `${err}`};
         res.writeHead(500, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
         res.write(JSON.stringify(msg));
         res.end(null);
+        setTimeout(() => busy.splice(busy.indexOf(req.connection.remoteAddress), 1), COOLDOWN);
+
         
       });
     }
-  }).listen(process.env.PORT);
+  }).listen(PORT);
 }
 
 main();
-console.log(`[OK] Listening on the port ${process.env.PORT}`);
+console.log(`[OK] Listening on the port ${PORT}`);
