7dc29bd05d45dd1f554128eebd25dca81ce66201
Nelly_ <6546381-NellyD3v@users.noreply.gitlab.com>
2020-10-08 01:26:43 +0700

    Add usage example written in Python

---
+++ usage_example/main.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Python 3 example, uses `requests` and `Pillow`
+"""
+
+import requests
+from io import BytesIO
+from PIL import Image
+
+target_website = "google.com"
+url = f"http://localhost:5000?url={target_website}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+  im = Image.open(BytesIO(response.content))
+  im.show()
+
+else:
+  print("Error:", data["content"])
